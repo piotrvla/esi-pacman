a353741caeec9932c4a71f14a63d3b7430090570
56212 <56212@etu.he2b.be>
2021-04-07 14:42:12 +0200

    Étape 1.2: visualisation du labyrinthe

---
+++ js/visualizeMaze.js
@@ -0,0 +1,27 @@
+/**
+ * Automatically executes written functions inside,
+ * whenever the document is ready.
+ */
+$(document).ready(function () {
+    displayMaze();
+
+});
+/**
+ * Display maze's walls, also the pac dots on the game board.
+ */
+function displayMaze() {
+    for (let i = 0; i < RAW_MAZE.table.length; i++) {
+        for (let j = 0; j < RAW_MAZE.table[i].length; j++) {
+            let mazeTile = RAW_MAZE.table[i][j];
+            if (mazeTile == 1) {
+                $("#game").append(`<p class='wall' 
+                style='top:${15 * i}px; left:${15 * j}px; '></p>`);
+            }
+            if (mazeTile == 2) {
+                $("#game").append(`<p class='pacdot' 
+                style='top:${15 * i}px; left:${15 * j}px; '></p>`);
+            }
+
+        }
+    }
+}
