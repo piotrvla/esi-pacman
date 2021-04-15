f8b2f6eaa78250c0ae12e73665a27ff0ae730f31
56212 <56212@etu.he2b.be>
2021-04-15 22:00:24 +0200

    étape 3.1 : fantômes

--- model/game.js
+++ model/game.js
@@ -8,7 +8,14 @@ class Game {
      */
     constructor(rawMaze) {
         this._game = new Maze(rawMaze);
-        this._pacman = new Pacman((this._game.pacmanRespawn), Direction.WEST);
+        this._pacman = new Pacman(this._game.pacmanRespawn, Direction.WEST);
+        this._blinky = new Ghost(this._game._ghostRespawn, Direction.NORTH, "Blinky");
+        this._pinky = new Ghost(this._game._ghostRespawn, Direction.SOUTH, "Pinky");
+        this._inky = new Ghost(this._game._ghostRespawn, Direction.EAST, "Inky");
+        this._clyde = new Ghost(this._game._ghostRespawn, Direction.WEST, "Clyde")
+        this._ghosts = [this._blinky, this._pinky, this._inky, this._clyde];
+
+
     }
     /**
      * Changes the direction of pacman if needed,
@@ -16,9 +23,25 @@ class Game {
      * if it's the case..
      */
     moveSprites() {
-        this.pacman.changeDirection();
-        if(this._game.canWalkOn(this._pacman.position.nextPosition(this._pacman.direction))){
-        this._pacman.move();
+
+
+        if (this._pacman._askedToChangeDirection && this._game.canWalkOn(this._pacman.position.nextPosition(this._pacman._askedDirection))) {
+            this.pacman.changeDirection();
+            this._pacman.move();
+        }
+        else if (this._game.canWalkOn(this._pacman.position.nextPosition(this._pacman.direction))) {
+            this._pacman.move();
+        }
+        for (let ghost of this._ghosts) {
+            if (this._game.canWalkOn(ghost.position.nextPosition(ghost.direction))) {
+                ghost.move();
+            }
+            else {
+                ghost.notifyIsBlocked();
+            }
+            if (ghost.canEat(this.pacman)) {
+                console.log("Pacman eated by a ghost");
+            }
         }
     }
     /**
